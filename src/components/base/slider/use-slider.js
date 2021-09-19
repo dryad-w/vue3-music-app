cf6746d8648fa9c2b8dd3b81d094e3ea5cdd3b13
dryad <kunbowu@foxmail.com>
2021-09-19 22:13:57 +0800

    推荐页面添加轮播图和推荐歌单

---
+++ src/components/base/slider/use-slider.js
@@ -0,0 +1,35 @@
+import BScroll from '@better-scroll/core'
+import Slide from '@better-scroll/slide'
+import { onMounted, onUnmounted, ref } from 'vue'
+
+BScroll.use(Slide)
+
+export default function useSlider(wrapperRef) {
+  const slider = ref(null)
+  const currentPageIndex = ref(0)
+
+  onMounted(() => {
+    slider.value = new BScroll(wrapperRef.value, {
+      click: true,
+      scrollX: true,
+      scrollY: false,
+      momentum: false,
+      bounce: false,
+      probeType: 2,
+      slide: true
+    })
+
+    slider.value.on('slideWillChange', (page) => {
+      currentPageIndex.value = page.pageX
+    })
+  })
+
+  onUnmounted(() => {
+    slider.value.destroy()
+  })
+
+  return {
+    slider,
+    currentPageIndex
+  }
+}
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,28 +1,120 @@
 <template>
   <div class="recommend">
-    推荐页面
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <Slider  v-if="sliders.length" :sliders="sliders"></Slider>
+      </div>
+    </div>
+    <div class="recommend-list">
+      <h1 class="list-title">热门歌单推荐</h1>
+      <ul>
+        <li
+          v-for="item in albums"
+          class="item"
+          :key="item.id"
+        >
+          <div class="icon">
+            <img width="60" height="60" :src="item.pic">
+          </div>
+          <div class="text">
+            <h2 class="name">
+              {{item.username}}
+            </h2>
+            <p class="title">
+              {{item.title}}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted } from 'vue'
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider.vue'
 
-export default defineComponent({
-  setup() {
-    // async function getRecommend1() {
-    //   const result = await getRecommend()
-    //   console.log(result)
-    // }
-    // getRecommend1()
-    onMounted(async () => {
-      const result = await getRecommend()
-      console.log(result)
-    })
+export default {
+  name: 'recommend',
+  components: {
+    Slider
+  },
+  data() {
+    return {
+      sliders: [],
+      albums: []
+    }
+  },
+  async created() {
+    const result = await getRecommend()
+    console.log(result)
+    this.sliders = result.sliders
+    this.albums = result.albums
   }
-})
+}
 </script>
 
 <style lang='scss' scoped>
+  .recommend {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .slider-wrapper {
+      position: relative;
+      width: 100%;
+      height: 0;
+      // 利用padding-top的百分比是基于父元素的宽度
+      padding-top: 40%;
+      overflow: hidden;
+      .slider-content {
+        // 绝对定位是基于上一层定位元素
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
 
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+        }
+
+        .name {
+          margin-bottom: 10px;
+          color: $color-text;
+        }
+
+        .title {
+          color: $color-text-d;
+        }
+      }
+    }
+  }
 </style>
