d51d83e3b4ab9700cfa5e8b2a7fbda2888bb6d22
dryad <kunbowu@foxmail.com>
2021-09-20 15:14:18 +0800

    推荐页面基本完毕，增加了v-loading自定义指令和v-lazy图片懒加载

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,48 +1,60 @@
 <template>
-  <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <Slider  v-if="sliders.length" :sliders="sliders"></Slider>
+  <div class="recommend"  v-loading:[loadingText]="loading">
+    <Scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <Slider  v-if="sliders.length" :sliders="sliders"></Slider>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
+          <ul>
+            <li
+              v-for="item in albums"
+              class="item"
+              :key="item.id"
+            >
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic">
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{item.username}}
+                </h2>
+                <p class="title">
+                  {{item.title}}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
-      <ul>
-        <li
-          v-for="item in albums"
-          class="item"
-          :key="item.id"
-        >
-          <div class="icon">
-            <img width="60" height="60" :src="item.pic">
-          </div>
-          <div class="text">
-            <h2 class="name">
-              {{item.username}}
-            </h2>
-            <p class="title">
-              {{item.title}}
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import { getRecommend } from '@/service/recommend'
 import Slider from '@/components/base/slider/slider.vue'
+import Scroll from '@/components/base/scroll/scroll.vue'
 
 export default {
   name: 'recommend',
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   data() {
     return {
       sliders: [],
-      albums: []
+      albums: [],
+      loadingText: '正在载入歌单...'
+    }
+  },
+  computed: {
+    loading() {
+      return !this.sliders.length && !this.albums.length
     }
   },
   async created() {
@@ -60,59 +72,63 @@ export default {
     width: 100%;
     top: 88px;
     bottom: 0;
-    .slider-wrapper {
-      position: relative;
-      width: 100%;
-      height: 0;
-      // 利用padding-top的百分比是基于父元素的宽度
-      padding-top: 40%;
+    .recommend-content {
+      height: 100%;
       overflow: hidden;
-      .slider-content {
-        // 绝对定位是基于上一层定位元素
-        position: absolute;
-        left: 0;
-        top: 0;
+      .slider-wrapper {
+        position: relative;
         width: 100%;
-        height: 100%;
-      }
-    }
-    .recommend-list {
-      .list-title {
-        height: 65px;
-        line-height: 65px;
-        text-align: center;
-        font-size: $font-size-medium;
-        color: $color-theme;
-      }
-      .item {
-        display: flex;
-        box-sizing: border-box;
-        align-items: center;
-        padding: 0 20px 20px 20px;
-
-        .icon {
-          flex: 0 0 60px;
-          width: 60px;
-          padding-right: 20px;
+        height: 0;
+        // 利用padding-top的百分比是基于父元素的宽度
+        padding-top: 40%;
+        overflow: hidden;
+        .slider-content {
+          // 绝对定位是基于上一层定位元素
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
         }
-
-        .text {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          flex: 1;
-          line-height: 20px;
-          overflow: hidden;
+      }
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
           font-size: $font-size-medium;
+          color: $color-theme;
         }
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
 
-        .name {
-          margin-bottom: 10px;
-          color: $color-text;
-        }
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
 
-        .title {
-          color: $color-text-d;
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+          }
+
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+
+          .title {
+            color: $color-text-d;
+          }
         }
       }
     }
