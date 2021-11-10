7aa230cfb3ffd3f6d95c7edb1c4dc123c5dd7325
dryad <kunbowu@foxmail.com>
2021-11-10 23:13:04 +0800

    歌单详情

---
+++ src/views/album.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="album">
+    <music-list
+      :songs="songs"
+      :title="title"
+      :pic="pic"
+      :loading="loading"
+    ></music-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'album'
+}
+</script>
+
+<style lang="scss" scoped>
+  .album {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: $color-background;
+  }
+</style>
